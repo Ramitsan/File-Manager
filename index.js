a85f6a3a44d4ca2a24c641c77d1a82ad401964e7
Ramitsan <ramitsan@gmail.com>
2023-06-24 23:38:35 +0300

    feat: implement work with relative and absolute paths

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import path from 'path';
+import { cd, ls, readAndPrintFile, createEmptyFile, renameFile, copyFile, moveFile, deleteFile } from './file-system.js';
 
 const parseArgs = () => {
   const args = {};
@@ -15,19 +15,85 @@ const args = parseArgs();
 console.log(args);
 const commands = {
   cd: (params) => {
-
+    const cdPath = params[0];
+    cd(cdPath);
   },
   ls: (params) => {
+    ls();
+  }, 
 
+  cat: (params) => {
+    readAndPrintFile(params);    
+  },
+
+  add: (params) => {
+    createEmptyFile(params);    
+  },  
+  
+  rn: (params) => {
+    const pathToFile = params.split(' ')[0];
+    const newFilename = params.split(' ')[1];
+    renameFile(pathToFile, newFilename);
+  },
+
+  cp: (params) => {
+    const pathToFile = params.split(' ')[0];
+    const pathToNewDirectory = params.split(' ')[1];
+
+    copyFile(pathToFile, pathToNewDirectory);
+  },
+
+  mv: (params) => {
+    const pathToFile = params.split(' ')[0];
+    const pathToNewDirectory = params.split(' ')[1];
+
+    moveFile(pathToFile, pathToNewDirectory);
+  },
+
+  rm: (params) => {
+    deleteFile(params);
   }
 }
 
+const splitParams = (params) => {
+  const result = [];
+  let counter = false;
+  let value = '';
+  params.split('').forEach(it => {
+    if(it == '\'') {
+      counter = !counter;
+    } else if(((/[ \n\r]/).test(it)) && (counter == false)) {
+      result.push(value);
+      value = '';
+    } else {
+      value += it;
+    }
+  })
+  if(value) {
+    result.push(value);
+  }
+  return result;
+}
+
+console.log(splitParams('xfojgfidpj'));
+console.log(splitParams('xfoj gfidpj'));
+console.log(splitParams('\'xfoj\' \'gfidpj\''));
+console.log(splitParams('\'xf oj\' \'gfi dpj\''));
+
 process.stdin.on('data', (data) => {
-  const command = data.toString().split(' ')[0];
-  const params = data.toString().slice(command.length);
-  console.log(command, params);
+  console.log(data.toString().split(/[ \n\r]/));
+  // const commandList = data.toString().split(/[ \n\r]/).filter(it => it);
+  const commandList = splitParams(data.toString()).filter(it => it);
+
+
+  const command = commandList[0];
+  const params = commandList.slice(1);
+
+  // console.log('++', command, '++');
+  // console.log('--', params, '--');
+
   const commandHandler = commands[command];
-  if(commandHandler) {
+  if (commandHandler) {
     commandHandler(params);
   } else {
     console.log('unknown command')
@@ -39,3 +105,5 @@ process.on('SIGINT', () => {
   console.log('close');
   process.exit();
 })
+
+// console.log(process.env);
